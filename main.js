6ee05b23371068ba21b262d58706e2679cd5cea1
Dhruv786Sharma <115816524+Dhruv786Sharma@users.noreply.github.com>
2023-09-05 13:42:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+function qus1() {
+    var x = document.getElementById("q1");
+    
+    if (x.style.display === "none") {
+      q.style.display = "block";
+      
+      
+    } else {
+      x.style.display = "none";
+      console.log("right");
+    }
+  }
+  function wr1() {
+    var x = document.getElementById("q1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      
+    
+    }
+ else {
+    x.style.display = "none";
+    console.log("worng");
+  }
+ 
+}
+
+function qus2() {
+    var w = document.getElementById("q2");
+    if (w.style.display === "none") {
+      w.style.display = "block";
+    } else {
+      w.style.display = "none";
+      console.log("right");
+    }
+  }
+  function wr2() {
+    var w = document.getElementById("q2");
+    if (w.style.display === "none") {
+      w.style.display = "block";
+    
+    }
+ else {
+    w.style.display = "none";
+    console.log("worng");
+  }
+ 
+}
+
+function qus3() {
+    var e = document.getElementById("q3");
+    if (e.style.display === "none") {
+      e.style.display = "block";
+    } else {
+      e.style.display = "none";
+      console.log("right");
+    }
+  }
+  function wr3() {
+    var e = document.getElementById("q3");
+    if (e.style.display === "none") {
+      e.style.display = "block";
+    
+    }
+ else {
+    e.style.display = "none";
+    console.log("worng");
+  }
+ 
+}
+
+function qus4() {
+    var r = document.getElementById("q4");
+    if (r.style.display === "none") {
+      r.style.display = "block";
+    } else {
+      r.style.display = "none";
+      console.log("right");
+    }
+  }
+  function wr4() {
+    var r = document.getElementById("q4");
+    if (r.style.display === "none") {
+      r.style.display = "block";
+    
+    }
+ else {
+    r.style.display = "none";
+    console.log("worng");
+  }
+ 
+}
+
+function qus5() {
+    var t = document.getElementById("q5");
+    if (t.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      t.style.display = "none";
+      console.log("right");
+      window.open("https://images.indianexpress.com/2020/07/guru-purnima-5.jpg?resize=150");
+    }
+  }
+  function wr5() {
+    var t = document.getElementById("q5");
+    if (t.style.display === "none") {
+      t.style.display = "block";
+    
+    }
+ else {
+    t.style.display = "none";
+    console.log("worng");
+    window.open("https://images.indianexpress.com/2020/07/guru-purnima-5.jpg?resize=150");
+  }
+ 
+}
